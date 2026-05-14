f389599131cfa20d6ba488db83d0fdc409be20f2
Michael Völkl <michaelvoelkl@zoho.com>
2026-05-14 16:36:42 +0200

    cargo fmt

--- hexagon_backend/src/routes.rs
+++ hexagon_backend/src/routes.rs
@@ -25,7 +25,9 @@ pub fn router(state: AppState) -> Router {
     let cors = CorsLayer::new()
         .allow_origin([
             "http://localhost:8080".parse::<HeaderValue>().unwrap(),
-            "https://hexagon-game-a0w8.onrender.com".parse::<HeaderValue>().unwrap(),
+            "https://hexagon-game-a0w8.onrender.com"
+                .parse::<HeaderValue>()
+                .unwrap(),
         ])
         .allow_methods([
             axum::http::Method::GET,
