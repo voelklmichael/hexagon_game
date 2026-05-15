1d117a4b7681c99a33508d8a4a19dd8a626c950c
Michael Völkl <michaelvoelkl@zoho.com>
2026-05-15 22:50:15 +0200

    correcting login behaviour

--- hexagon_egui/src/app/backend_responses.rs
+++ hexagon_egui/src/app/backend_responses.rs
@@ -2,6 +2,20 @@ use crate::HexApp;
 
 impl HexApp {
     pub(super) fn process_backend_responses(&mut self) {
+        if let Some((id, name)) = self.backend_reqwest.session_user.take() {
+            self.user_login.logged_in_as = Some((id, name));
+            if !self.backend_reqwest.fetch_mission_user_best_task.is_pending() {
+                if let (Some((user_id, _)), Some(uuid)) = (
+                    &self.user_login.logged_in_as,
+                    self.current_mission
+                        .and_then(|idx| crate::panels::missions::mission_id(&self.missions, idx)),
+                ) {
+                    self.backend_reqwest
+                        .fetch_mission_user_best(*user_id, uuid);
+                }
+            }
+        }
+
         if let Some(result) = self.backend_reqwest.fetch_all_missions_task.take() {
             self.missions_loaded = true;
             match result {
